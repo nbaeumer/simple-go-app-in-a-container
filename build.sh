2eb9931144729260b93355269923aae57d6183d9
Nico Baeumer <nbaeumer@amazon.de>
2018-06-05 09:58:35 +0200

    Update structure

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 VERSION=v3
 
-CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o simple-app ./main.go
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ./go/simple-app ./go/main.go
 docker build -t simpleapp-scratch:$VERSION .
 docker images
 rm simple-app
